abbb3461ea6af8b4c1a7f8a640d1bcdabbeb88b9
yuvalni <original.pr@gmail.com>
2022-06-09 14:20:49 +0300

    strating to form

---
+++ main.py
@@ -0,0 +1,101 @@
+import imgui
+import glfw
+import OpenGL.GL as gl
+from imgui.integrations.glfw import GlfwRenderer
+from pymeasure.instruments.keithley import Keithley2400
+
+
+def impl_glfw_init(window_name="minimal ImGui/GLFW3 example", width=1280, height=720):
+    if not glfw.init():
+        print("Could not initialize OpenGL context")
+        exit(1)
+
+    # OS X supports only forward-compatible core profiles from 3.2
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
+
+    # Create a windowed mode window and its OpenGL context
+    window = glfw.create_window(int(width), int(height), window_name, None, None)
+    glfw.make_context_current(window)
+
+    if not window:
+        glfw.terminate()
+        print("Could not initialize Window")
+        exit(1)
+
+    return window
+
+
+class GUI(object):
+
+    def __init__(self):
+        super().__init__()
+        self.backgroundColor = (0, 0, 0, 0.5)
+        self.window = impl_glfw_init(width=700, height=500)
+        gl.glClearColor(*self.backgroundColor)
+        imgui.create_context()
+        self.impl = GlfwRenderer(self.window)
+        self.keithley = Keithley2400("GPIB::16")
+        self.initKeithley()
+        self.output = False
+        self.current = 0.0
+        self.loop()
+
+    def initKeithley(self):
+        self.keithley.reset()
+        # setting current params
+        self.keithley.use_front_terminals()
+        self.keithley.apply_current(0.01, 2.0)
+        self.keithley.wires = 4  # set to 4 wires
+        # self.keithley.compliance_voltage = V_comp
+        self.keithley.source_current = 0
+
+        # setting voltage read params
+        self.keithley.measure_voltage(5, 2, False)
+        #just for fun:
+        self.keithley.beep(400, 1)
+        self.keithley.write(":SYST:BEEP:STAT OFF")
+
+
+
+    def loop(self):
+        while not glfw.window_should_close(self.window):
+            glfw.poll_events()
+            self.impl.process_inputs()
+            imgui.new_frame()
+            imgui.begin("Keithley GUI", True)
+
+
+            imgui.text("Current control")
+
+            changed, self.current = imgui.input_double('Applied current [mA]', self.current)
+            if changed:
+                self.keithley.source_current = self.current/1000
+                assert self.keithley.source_current < 0.01
+                # need to do something more gentle
+            _, self.output = imgui.checkbox("Apply current", self.output)
+            if _:
+                if self.output:
+                    self.keithley.enable_source()
+                else:
+                    self.keithley.disable_source()
+
+            imgui.end()
+
+            imgui.render()
+
+            gl.glClearColor(*self.backgroundColor)
+            gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+            self.impl.render(imgui.get_draw_data())
+            glfw.swap_buffers(self.window)
+
+        self.impl.shutdown()
+        glfw.terminate()
+
+
+if __name__ == "__main__":
+    gui = GUI()
